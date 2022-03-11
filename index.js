c811350aaa496b91137dbc314cf2978f4dde782d
ItaloCobains <monokatchaleger@gmail.com>
2022-03-10 22:43:47 -0300

    algumas atualizacoes

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 "use strict";
-const Discord = require("discord.js");
-
-const client = new Discord.Client();
+require("dotenv").config();
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
+const TOKEN = process.env.TOKEN;
+const PREFIX = process.env.PREFIX;
 
 client.on("ready", () => {
-  console.info("Aplicativo aberto");
+  console.info("app aberto");
 });
 
-client.login("OTUxNjMyMjk4ODIwOTAyOTQy.YiqSpA.e2reDXAYTdatPP2aosvOXZ3TPeg");
+client.on("message", (message) => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  const command = message.content.split(PREFIX)[1];
+
+  if (command === "ola") message.channel.send("Ola");
+  else if (command == "msg Colorida") {
+    const embed = new MessageEmbed()
+      .setTitle("Testando mensagem randola")
+      .setColor("oxff0000")
+      .setDescription("kenny e gay\n Adriano tbm")
+      .setAuthor('I')
+    message.channel.send(embed);
+  }
+});
+
+client.login(TOKEN);
